3277bde40314bff2c64810aafe76982aafeb2bb9
AhmedGamal172000 <ahmedgamal172000@gmail.com>
2022-05-17 16:37:02 +0200

    Handling main to be less in lines

--- Project_2_StopWatch/Project_2_StopWatch.c
+++ Project_2_StopWatch/Project_2_StopWatch.c
@@ -2,6 +2,7 @@
  *Diploma 64
  *PROJECT 2
  */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -16,8 +17,60 @@ unsigned char MIN0  = 0;
 unsigned char MIN1  = 0;
 unsigned char HOUR0 = 0;
 unsigned char HOUR1 = 0;
+unsigned int arr[]={SEC0, SEC1, MIN0 , MIN1, HOUR0,HOUR1};
 
 
+
+int display();
+ISR(TIMER1_COMPA_vect);
+ISR(INT0_vect);
+ISR(INT1_vect);
+ISR(INT2_vect);
+void Int0Handle();
+void Int1Handle();
+void Int2Handle();
+void Timer1_CTC_Init(void);
+
+int main(void)
+{
+	DDRC |= 0x0F;
+	PORTC = 0xf0;
+
+	DDRA |= 0x3F;
+
+	SREG|=(1<<7); //Enable global interrupt
+
+	Timer1_CTC_Init(); //start timer1
+	Int0Handle();
+	Int1Handle();
+	Int2Handle();
+	while(1)
+	{
+
+		display();
+	}
+}
+int display()
+{
+
+	static char i = 0;
+	if(i == 6)
+	{
+		i =0;
+		return 0;
+	}
+	else
+	{
+		PORTC = (PORTC & 0xF0) | (arr[i] & 0x0F);
+		_delay_us(delay);
+		PORTA |= (1<<i);
+		_delay_us(delay);
+		PORTA &= ~(1<<i);
+		//PA0 -- PA5
+		i++;
+		return 0;
+	}
+}
 ISR(TIMER1_COMPA_vect)
 {
 	tick++;
@@ -110,66 +163,4 @@ void Timer1_CTC_Init(void)
 
 
 	TCCR1B = (1<<WGM12) | (1<<CS10) |(1<<CS12); //2- CLK/1 .. CTC mode WGM12
-}
-
-
-
-int main(void)
-{
-	DDRC |= 0x0F;
-	PORTC = 0xf0;
-
-	DDRA |= 0x3F;
-
-	SREG|=(1<<7); //Enable global interrupt
-
-	Timer1_CTC_Init(); //start timer1
-	Int0Handle();
-	Int1Handle();
-	Int2Handle();
-	while(1)
-	{
-		//digit 0
-		PORTC = (PORTC & 0xF0) | (SEC0 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA0);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA0);
-
-		//digit 1
-		PORTC = (PORTC & 0xF0) | (SEC1 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA1);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA1);
-
-		//digit 2
-		PORTC = (PORTC & 0xF0) | (MIN0 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA2);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA2);
-
-		//digit 3
-		PORTC = (PORTC & 0xF0) | (MIN1 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA3);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA3);
-
-		//digit 4
-		PORTC = (PORTC & 0xF0) | (HOUR0 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA4);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA4);
-
-		//digit 5
-		PORTC = (PORTC & 0xF0) | (HOUR1 & 0x0F);
-		_delay_us(delay);
-		PORTA |= (1<<PA5);
-		_delay_us(delay);
-		PORTA &= ~(1<<PA5);
-
-	}
 }
